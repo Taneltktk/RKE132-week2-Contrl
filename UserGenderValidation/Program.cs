daac46dc3699e355766bc0f73629cf6f9e099adb
TanelP <tanel.pool@tktk.ee>
2024-09-22 20:09:56 +0300

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading.Channels;
+
+Console.WriteLine("Hello, Tanel!");
+
+//rakendus küsib kasutajalt tema sugu (m/f)
+//rakendus küsib kasutajalt tema perekonnanime
+//lähtudes kasutaja valikust, tervitab rakendus kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]
+
+Console.WriteLine("Please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+
+Console.WriteLine("Please, enter your surname:");
+string surName = Console.ReadLine();
+
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {surName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {surName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {surName}!");
+}
+
+
